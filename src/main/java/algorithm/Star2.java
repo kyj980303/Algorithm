1f9c1f8f6abc01387ba0297cc2bd0963983c68d7
kyj980303 <rladuswjd0303@naver.com>
2022-09-13 18:18:06 +0900

    우측정렬해서 별찍기

---
+++ src/main/java/algorithm/Star2.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class Star2 {
+  public static void main(String[] args) {
+
+    Scanner keyboard = new Scanner(System.in);
+    int num = keyboard.nextInt();
+
+    for (int i = 1; i <= num; i++) {
+
+      for (int j = num; j > 0; j--) {
+        if (i < j) {
+
+          System.out.print(" ");
+        } else {
+
+          System.out.print("*");
+        }
+      }
+      System.out.println("");
+    }
+  }
+}
