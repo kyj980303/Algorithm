7c1642b67e6c909903d551d3389ab5ca5bc3cd26
kyj980303 <rladuswjd0303@naver.com>
2022-09-13 09:37:03 +0900

    두 수를 입력받아 크기 비교하기

--- src/main/java/algorithm/compare.java
+++ src/main/java/algorithm/compare.java
@@ -1,24 +1,19 @@
-// 두 정수 A와 B가 주어졌을 때, A와 B를 비교하는 프로그램을 작성하시오.
-// 첫째 줄에 A와 B가 주어진다. A와 B는 공백 한 칸으로 구분되어져 있다.
-// -10,000 ≤ A, B ≤ 10,000
 
 public class compare {
   public static void main(String[] args) {
     java.util.Scanner keyNum = new java.util.Scanner(System.in);
 
-    String num = keyNum.nextLine();
-    String[] splitNum = num.split("\\s+");
-    int A = Integer.parseInt(splitNum[0]);
-    int B = Integer.parseInt(splitNum[1]);
+    int A = keyNum.nextInt();
+    int B = keyNum.nextInt();
 
     if (A >= -10000 && A <= 10000) {
-      if (B >= -10000 && B <= 1000) {
+      if (B >= -10000 && B <= 10000) {
         if (A > B) {
-          System.out.println(" > ");
+          System.out.println(">");
         } else if (A < B) {
-          System.out.println(" < ");
+          System.out.println("<");
         } else {
-          System.out.println(" = ");
+          System.out.println("==");
         }
       }
     }
