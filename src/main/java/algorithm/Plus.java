2ee4057016db6a30ccaa6786c263a2b5c2562afa
kyj980303 <rladuswjd0303@naver.com>
2022-09-15 19:37:33 +0900

    두 정수 A와 B를 입력받은 다음 A + B 를 출력하시오

---
+++ src/main/java/algorithm/Plus.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Plus {
+  public static void main(String[] args) {
+
+    Scanner in = new Scanner(System.in);
+    int num = in.nextInt();
+    int[] arr = new int[num];
+
+    int result = 0;
+
+    for (int i = 0; i < num; i++) {
+
+      int num2 = in.nextInt();
+      int num3 = in.nextInt();
+
+      arr[i] = num2 + num3;
+
+    }
+    for (int k : arr) {
+      System.out.println(k);
+    }
+
+  }
+}
