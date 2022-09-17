25eaf6c50d55d04c4168b15a81491dfc3d6be3d1
kyj980303 <rladuswjd0303@naver.com>
2022-09-17 18:37:39 +0900

    알람시계

---
+++ src/main/java/algorithm/Alram.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Alram {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+
+    int time = in.nextInt();
+    int min = in.nextInt();
+    int minute = 0;
+
+
+    if (min >= 45 && min < 60) {
+      minute = min - 45;
+
+      System.out.printf("%d %d", time, minute);
+    } else {
+
+      if (time == 0) {
+        time = 24;
+      }
+
+      minute = min + 15;
+      --time;
+
+      System.out.printf("%d %d", time, minute);
+    }
+
+  }  
+}
