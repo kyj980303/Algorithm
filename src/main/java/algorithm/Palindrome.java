400108f786a34ec2fcb72e462229fa70dd5f3516
kyj980303 <rladuswjd0303@naver.com>
2022-09-29 09:12:48 +0900

    팰린드롬수 판단하기

---
+++ src/main/java/algorithm/Palindrome.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Palindrome {
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    while (true) {
+      String S = br.readLine();
+      StringBuilder sb = new StringBuilder(S);
+      String Sreverse = sb.reverse().toString();
+
+      if (S.equals("0")) break;
+
+      if (S.equals(Sreverse)) {
+        System.out.println("yes");
+      } else {
+        System.out.println("no");
+      }
+    }
+  }
+}
