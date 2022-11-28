12449d8744317f8cfbf44e6ade01d0b24d9d1c0b
YJung <rladuswjd0303@naver.com>
2022-11-28 23:14:35 +0900

    백준 11651번 좌표 정렬하기2

---
+++ src/main/java/algorithm/Coordinate2.java
@@ -0,0 +1,33 @@
+// 2차원 평면 위의 점 N개가 주어진다. 좌표를 x좌표가 증가하는 순으로, 
+// x좌표가 같으면 y좌표가 증가하는 순서로 정렬한 다음 출력하는 프로그램을 작성하시오.
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Coordinate2 {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+
+    int num = in.nextInt();
+    int[][] arr = new int[num][2];
+
+    for (int i = 0; i < num; i++) {
+      arr[i][0] = in.nextInt();
+      arr[i][1] = in.nextInt();
+    }
+
+    Arrays.sort(arr, (i1, i2) -> {
+      if (i1[1] == i2[1]) {
+        return i1[0] - i2[0];
+      } else {
+        return i1[1] - i2[1];
+      }
+    });
+
+    for (int i = 0; i < num; i++) {
+      System.out.println(arr[i][0] + " " + arr[i][1]);
+    }
+
+    in.close();
+  }
+}
+
