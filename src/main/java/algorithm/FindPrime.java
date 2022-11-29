dd1cddfbb334c9695bcc7a81b0dd68b64292381a
YJung <rladuswjd0303@naver.com>
2022-11-29 22:20:39 +0900

    백준 1978번 소수찾기

---
+++ src/main/java/algorithm/FindPrime.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class FindPrime {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+
+    int num = in.nextInt();
+    int count = 0;
+
+    for (int i = 0; i < num; i++) {
+
+      boolean isPrime = true;
+
+      int N = in.nextInt();
+
+      if (N == 1) {
+        continue;
+      } 
+      for (int j = 2; j <= Math.sqrt(N); j++) {
+        if (N % j == 0) {
+          isPrime = false;
+          break;
+        }
+      }
+      if (isPrime) {
+        count++;
+      }
+    }
+    System.out.println(count);
+
+    in.close();
+  }
+}
