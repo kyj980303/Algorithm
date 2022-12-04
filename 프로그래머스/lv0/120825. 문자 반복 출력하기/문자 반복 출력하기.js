9441c0cfe1d824d4e1f09a12681b6d6e060343b9
Yeonjung Kim <rladuswjd0303@naver.com>
2022-12-04 15:52:23 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120825. 문자 반복 출력하기/문자 반복 출력하기.js
@@ -0,0 +1,15 @@
+function solution(my_string, n) {
+    let arr = my_string.split("");
+    let result = "";
+    
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < n; j++) {
+            arr[i];
+            result += arr[i];
+        }
+   
+        
+    }
+    
+    return result;
+}
