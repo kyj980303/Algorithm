f21cc69445ef097314a6c57029023017b90ebea0
Yeonjung Kim <rladuswjd0303@naver.com>
2022-12-06 16:52:40 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -0,0 +1,14 @@
+function solution(numbers) {
+    let result = [];
+    
+    for (let i = 0; i < numbers.length; i++) {
+        for (let j = 0; j < numbers.length; j++) {    
+            if (i != j) {
+                result.push(numbers[i] * numbers[j]);
+            }
+        }
+       
+    }
+    
+    return Math.max(...result);
+}
