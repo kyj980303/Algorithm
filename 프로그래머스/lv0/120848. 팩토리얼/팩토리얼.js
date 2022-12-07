1f3da0a8bdec2d5ed8cef8107638c47593067565
Yeonjung Kim <rladuswjd0303@naver.com>
2022-12-07 15:06:31 +0900

    [level 0] Title: 팩토리얼, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.js
@@ -0,0 +1,13 @@
+function solution(n) {
+    let result = 1;
+    for (let i = 1; i <= n; i++) {
+        result *= i;
+        
+        if (result === n) {
+            return i;
+        }
+        if (result > n) {
+            return i - 1;
+        }
+    }
+}
