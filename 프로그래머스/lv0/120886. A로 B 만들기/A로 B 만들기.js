c032945f583eaa1102ead89aa81419b41cc3d794
Yeonjung Kim <rladuswjd0303@naver.com>
2022-12-07 15:23:10 +0900

    [level 0] Title: A로 B 만들기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.js
@@ -0,0 +1,6 @@
+function solution(before, after) {
+    before = [...before].sort();
+    after = [...after].sort();
+    
+    return before.filter((a, i) => a === after[i]).length === after.length ? 1 : 0;
+}
