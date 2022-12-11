4d9ab32a82e4861b33d90b2fe19d3daab0f98639
Yeonjung Kim <rladuswjd0303@naver.com>
2022-12-11 15:39:45 +0900

    [level 0] Title: 외계어 사전, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.js
@@ -0,0 +1,5 @@
+function solution(spell, dic) {
+    spell = spell.sort().join("");
+    dic = dic.map(a => a.split("").sort().join("")).find(e => e === spell);
+    return dic !== undefined? 1 : 2;
+}
