b7c3ee8681654ad1b3d9e15d45b0e3b0d92004e4
Yeonjung Kim <rladuswjd0303@naver.com>
2022-12-12 16:19:49 +0900

    [level 0] Title: 로그인 성공?, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
@@ -0,0 +1,20 @@
+function solution(id_pw, db) {
+    
+    for (let i = 0; i < db.length; i++) {
+        
+        if (id_pw[0] === db[i][0] && id_pw[1] === db[i][1]) {
+            return "login";
+        } else if (id_pw[0] === db[i][0] && id_pw[1] != db[i][1]) {
+            return "wrong pw";
+        } 
+        
+    } 
+    
+    for (let i = 0; i < db.length; i++) {
+        if (id_pw[0] != db[i][0] && id_pw[1] != db[i][1]) {
+            return "fail";
+        }
+    }
+    
+    
+}
