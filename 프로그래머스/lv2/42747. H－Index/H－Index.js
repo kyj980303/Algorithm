e3eaaaa80a1e0948d3d3a8d519e44cca5d56eccf
Yeonjung Kim <rladuswjd0303@naver.com>
2023-01-07 16:57:12 +0900

    [level 2] Title: H-Index, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42747. H－Index/H－Index.js
@@ -0,0 +1,11 @@
+function solution(citations) {
+    citations = citations.sort((a, b) => b - a);
+    
+    for (let i = 0; i < citations.length; i++) {
+        if (citations[i] <= i) {
+            return i;
+        }
+    }
+    
+    return citations.length;
+}
