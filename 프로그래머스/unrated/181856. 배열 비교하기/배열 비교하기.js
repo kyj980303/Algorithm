54e8ea684d417856156a852f5c6bff001b9b91e2
Yeonjung Kim <rladuswjd0303@naver.com>
2023-08-26 17:02:56 +0900

    [unrated] Title: 배열 비교하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181856. 배열 비교하기/배열 비교하기.js
@@ -0,0 +1,21 @@
+function solution(arr1, arr2) {
+    if (arr1.length < arr2.length) {
+        return -1
+    } else if (arr1.length === arr2.length) {
+        let a = 0
+        let b = 0
+        for (let i = 0; i < arr1.length; i++) {
+            a += arr1[i]
+            b += arr2[i]
+        }
+        if (a < b) {
+            return -1
+        } else if (a > b) {
+            return 1
+        } else {
+            return 0
+        }
+    } else {
+       return 1
+    }
+}
