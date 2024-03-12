cbbbab3563aa31d22de746227e5ad941684387d0
Yeonjung Kim <rladuswjd0303@naver.com>
2024-03-13 01:47:34 +0900

    [level 2] Title: 전화번호 목록, Time: 99.90 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -5,7 +5,7 @@ function solution(phone_book) {
     for (let i = 0; i < phone_book.length - 1; i++) {
         // 접두어인지 확인
         // substring(n,k) => n번째부터 k - 1번째 까지 자름
-        if (phone_book[i] === phone_book[i + 1].substring(0, phone_book[i].length)) {
+        if (phone_book[i+1].startsWith(phone_book[i]) === true) {
           return false;
         }
     }
